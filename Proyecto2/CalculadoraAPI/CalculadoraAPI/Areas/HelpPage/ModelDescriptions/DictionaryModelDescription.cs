51aa9bc1d98e04ef2df960d3e4d2cd31b9a55411
Rolando Flores <rafj26@outlook.com>
2024-11-20 09:28:01 -0500

    Actualizacion RF

--- Proyecto2/CalculadoraAPI/CalculadoraAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CalculadoraAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Proyecto2/CalculadoraAPI/CalculadoraAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CalculadoraAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
